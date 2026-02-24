eb68cdd0fa67502e42a731eeb85a810100c40e9e
Noah <117038300+NoahCxrest@users.noreply.github.com>
2026-02-23 20:59:39 -0700

    Remove header comments from index.ts
    Removed file overview comments from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-/**
- * @fileoverview Melonly API Client - Official TypeScript SDK
- * @author Your Name
- * @version 1.0.0
- */
-
 export { MelonlyClient, type ClientOptions } from "./client";
 export {
   MelonlyError,
